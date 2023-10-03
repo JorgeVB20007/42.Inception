e884671a3e673ec6e60c344eff44e1d28d06531e
jvacaris <>
2023-10-03 16:50:39 +0200

    root

--- srcs/requirements/mariadb/tools/createdb.sh
+++ srcs/requirements/mariadb/tools/createdb.sh
@@ -16,7 +16,7 @@ export MYSQL_SERVER_NAME=jvacaris_db
 	echo "CREATE DATABASE IF NOT EXISTS $MYSQL_SERVER_NAME ;" > db1.sql
 	echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> db1.sql
 	echo "GRANT ALL PRIVILEGES ON $MYSQL_SERVER_NAME.* TO '$MYSQL_USER'@'%' ;" >> db1.sql
-	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
+	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '"$MYSQL_ROOT_PASSWORD"' ;" >> db1.sql
 	echo "FLUSH PRIVILEGES;" >> db1.sql
 	echo "db: (3)"
 	mysql < db1.sql
