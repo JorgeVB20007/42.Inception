097a120e25714395b01b24d1ba7ffda613b54493
jvacaris <>
2023-10-08 18:04:53 +0200

    Mariadb works

--- srcs/requirements/mariadb/tools/createdb.sh
+++ srcs/requirements/mariadb/tools/createdb.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 echo "~~~~~~ CREATEDB.SH ~~~~~~"
 echo "Working if jvacaris: >>$MYSQL_USER<<"
+echo "Working if P455w0rd_r00t: >>$MYSQL_ROOT_PASSWORD<<"
+echo "Working if P455w0rd: >>$MYSQL_PASSWORD<<"
+echo "Working if jvacaris_db: >>$MYSQL_SERVER_NAME<<"
 echo "db: (1)"
 
+#echo go to sleep...
+#sleep infinity
 
 if test ! -f MYSQL_INSTALLED; then
-export MYSQL_ROOT_PASSWORD=P455w0rd_r00t
-export MYSQL_USER=jvacaris
-export MYSQL_PASSWORD=P455w0rd
-export MYSQL_SERVER_NAME=jvacaris_db
-#	mkdir /var/run/mysqld
-#	touch /var/run/mysqld/mysqld.sock
+	echo Installing...
 	service mysql start;
 	echo "db: (2)"
 	echo "CREATE DATABASE IF NOT EXISTS $MYSQL_SERVER_NAME ;" > db1.sql
@@ -31,27 +31,7 @@ ls /var/run/mysqld
 echo "~~~~~"
 
 echo "db: (5)"
+echo Installed...
 mysqld
 echo "db: (6)"
 
-# mysql_install_db --user=root --password=$MYSQL_ROOT_PASSWORD
-# # mysql_upgrade --user=root --password=$MYSQL_ROOT_PASSWORD
-# service mariadb start
-# echo "db: (1)"
-# mysql -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_SERVER_NAME\`;"
-# echo "db: (2)"
-# mysql -e "CREATE USER IF NOT EXISTS \`$MYSQL_USER\`@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
-# echo "db: (3)"
-# mysql -e "GRANT ALL PRIVILEGES ON \`$MYSQL_SERVER_NAME\`.* TO \`$MYSQL_USER\`@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-# echo "db: (4)"
-# # mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-# mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');"
-# echo "db: (5)"
-# #echo "> \$MYSQL_ROOT_PASSWORD = $MYSQL_ROOT_PASSWORD"
-# mysql -u root --password=$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
-# echo "db: (6)"
-# mysqladmin -uroot -p$MYSQL_ROOT_PASSWORD shutdown
-# echo "db: (7)"
-# #service mariadb stop
-# exec mysqld_safe
-# echo "db: (8)"
